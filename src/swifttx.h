2bf5db29d14b5c3bd9d89e4d7cb1875ac82aa6a5
Nico Lucciola <nico.lucciola@gmail.com>
2018-07-17 22:48:21 +0200

    Revert "add missing function"
    This reverts commit a35532da880bc4e1d05f54946ed7539ad96974e4.

--- src/swifttx.h
+++ src/swifttx.h
@@ -46,8 +46,6 @@ int64_t CreateNewLock(CTransaction tx);
 
 bool IsIXTXValid(const CTransaction& txCollateral);
 
-void ReprocessBlocks(int nBlocks);
-
 // if two conflicting locks are approved by the network, they will cancel out
 bool CheckForConflictingLocks(CTransaction& tx);
 
